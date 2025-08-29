ac07435382c373c326a4c156dec458f411e6cef8
mattiadurso <mattiadurso98@gmail.com>
2025-08-29 23:49:40 +0200

    add support to no url

--- download_wrappers.py
+++ download_wrappers.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 # wrapper to download files from a URL
-wrappers_to_download = ['dedode']
+wrappers_to_download = ['sift']
 
 # load dict with links for download
 with open("download_wrappers.yaml", "r") as f:
@@ -13,17 +13,19 @@ with open("download_wrappers.yaml", "r") as f:
 # download original implementations
 for name in wrappers_source.keys():
     if name in wrappers_to_download or len(wrappers_to_download) == 0:
-        name_path = Path(name)
+        name_path = Path(f"wrappers/{name}")
         if not name_path.exists():
             name_path.mkdir(parents=True, exist_ok=True)
+
         for file_info in wrappers_source[name]:
-            url = file_info["url"]
+            url  = file_info["url"]
             dest = file_info["dest"]
 
-            if 'github' in url:
-                os.system(f"git clone --recursive {url} wrappers/{name_path}/{dest}")
-            else:
-                os.system(f"wget -P wrappers/{name_path}/{dest} {url}")
+            if url:
+                if 'github' in url:
+                    os.system(f"git clone --recursive {url} wrappers/{name_path}/{dest}")
+                else:
+                    os.system(f"wget -P wrappers/{name_path}/{dest} {url}")
 
             if file_info.get("submodules"):
                 for submodule in file_info["submodules"]:
