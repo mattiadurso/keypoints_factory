c253d218e274848de7bbd56023e21cd65bab4f9f
mattiadurso <mattiadurso98@gmail.com>
2025-08-31 00:40:31 +0200

    .

---
+++ bash/fix_aliked_custom_ops.sh
@@ -0,0 +1,14 @@
+FILE1="../methods/aliked/custom_ops/get_patches_cuda.cu"
+FILE2="../methods/aliked/custom_ops/get_patches.cpp"  # if it exists
+
+# backups
+cp "$FILE1" "$FILE1.bak"; [ -f "$FILE2" ] && cp "$FILE2" "$FILE2.bak"
+
+# ensure the macro uses x.is_cuda()
+sed -i 's/#define CHECK_CUDA(x).*/#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")/' "$FILE1"
+
+# clean up any accidental replacements like input.scalar_type().is_cuda()
+sed -i 's/\.scalar_type().is_cuda()/\.is_cuda()/g' "$FILE1" "$FILE2"
+
+# build
+cd ../methods/aliked/custom_ops && sh build.sh
