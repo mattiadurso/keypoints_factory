185a4a23fff23d1da0d62000cf1cede6d2e5ac52
mattiadurso <mattiadurso98@gmail.com>
2025-09-18 18:11:59 +0200

    simplified methodoutput and wrapper

--- wrappers/wrapper.py
+++ wrappers/wrapper.py
@@ -17,23 +17,20 @@ from abc import ABC, abstractmethod
 class MethodOutput:
     kpts: Tensor
     kpts_scores: Optional[Tensor] = None
-    kpts_sizes: Optional[Tensor] = None  # ? receptive field
+    kpts_sizes: Optional[Tensor] = None  # receptive field
     kpts_scales: Optional[Tensor] = (
-        None  # ? at which resolution they have been extracted
+        None  # at which resolution they have been extracted, for multiscale
     )
     kpts_angles: Optional[Tensor] = None
     des: Optional[Tensor] = None
-    des_scores: Optional[Tensor] = None
-    det_map: Optional[Tensor] = None
-    det_map_proc: Optional[Tensor] = None
     des_vol: Optional[Tensor] = None
-    side_map: Optional[Tensor] = None
 
     def __post_init__(self):
         assert (
             self.kpts.ndim == 2
         ), f"kpts must have shape (N, 2), got {self.kpts.shape}"
-        # ? put emtpy stuff in place of None
+
+        # put emtpy stuff in place of None
         if self.kpts_scores is None:
             self.kpts_scores = torch.ones_like(self.kpts[:, 0])
         if self.kpts_sizes is None:
@@ -42,16 +39,6 @@ class MethodOutput:
             self.kpts_scales = torch.ones_like(self.kpts[:, 0])
         if self.kpts_angles is None:
             self.kpts_angles = torch.zeros_like(self.kpts[:, 0])
-
-    def cpu(self) -> MethodOutput:
-        return self.to("cpu")
-
-    def to(self, device: str) -> MethodOutput:
-        for field in fields(self):
-            tensor: Optional[Tensor] = getattr(self, field.name)
-            if tensor is not None:
-                setattr(self, field.name, tensor.to(device))
-        return self
 
     def __getitem__(self, key: Union[str, None]) -> Tensor:
         return self.__dict__[key]
@@ -71,78 +58,8 @@ class MethodOutput:
             kpts_scales=self.kpts_scales.clone()[mask],
             kpts_angles=self.kpts_angles.clone()[mask],
             des=self.des.clone()[mask] if self.des is not None else None,
-            des_scores=(
-                self.des_scores.clone()[mask] if self.des_scores is not None else None
-            ),
-            det_map=self.det_map.clone() if self.det_map is not None else None,
-            det_map_proc=(
-                self.det_map_proc.clone() if self.det_map_proc is not None else None
-            ),
             des_vol=self.des_vol.clone() if self.des_vol is not None else None,
-            side_map=self.side_map.clone() if self.side_map is not None else None,
         )
-
-    def slice(self, n_kpts: int) -> MethodOutput:
-        n = min(n_kpts, self.kpts.shape[0])
-        return MethodOutput(
-            kpts=self.kpts.clone()[:n],
-            kpts_scores=self.kpts_scores.clone()[:n],
-            kpts_sizes=self.kpts_sizes.clone()[:n],
-            kpts_scales=self.kpts_scales.clone()[:n],
-            kpts_angles=self.kpts_angles.clone()[:n],
-            des=self.des.clone()[:n] if self.des is not None else None,
-            des_scores=(
-                self.des_scores.clone()[:n] if self.des_scores is not None else None
-            ),
-            det_map=self.det_map.clone() if self.det_map is not None else None,
-            det_map_proc=(
-                self.det_map_proc.clone() if self.det_map_proc is not None else None
-            ),
-            des_vol=self.des_vol.clone() if self.des_vol is not None else None,
-            side_map=self.side_map.clone() if self.side_map is not None else None,
-        )
-
-    def save_as_h5(self, path: Path | str, tag: str = "") -> None:
-        path.mkdir(parents=True, exist_ok=True)
-        # ? save the keypoints as h5py
-        with h5py.File(path / f"{tag}keypoints.h5", "w") as fp:
-            fp.create_dataset("keypoints", data=self.kpts.cpu().numpy())
-            fp.create_dataset("scores", data=self.kpts_scores.cpu().numpy())
-            fp.create_dataset("sizes", data=self.kpts_sizes.cpu().numpy())
-            fp.create_dataset("scales", data=self.kpts_scales.cpu().numpy())
-            fp.create_dataset("angles", data=self.kpts_angles.cpu().numpy())
-
-        # ? save the descriptors as h5py
-        if self.des is not None:
-            with h5py.File(path / f"{tag}descriptors.h5", "w") as fp:
-                fp.create_dataset("descriptors", data=self.des.cpu().numpy())
-
-    @staticmethod
-    def load_from_h5(path: Path, tag: str = "") -> MethodOutput:
-        kpts_path = path / f"{tag}keypoints.h5"
-        assert kpts_path.exists(), f"keypoints file {kpts_path} does not exist"
-        with h5py.File(kpts_path, "r") as fp:
-            output = MethodOutput(
-                kpts=torch.from_numpy(fp["keypoints"][()]),
-                kpts_scores=torch.from_numpy(fp["scores"][()]),
-                kpts_sizes=torch.from_numpy(fp["sizes"][()]),
-                kpts_scales=torch.from_numpy(fp["scales"][()]),
-                kpts_angles=torch.from_numpy(fp["angles"][()]),
-            )
-
-        des_path = path / f"{tag}descriptors.h5"
-        if des_path.exists():
-            with h5py.File(des_path, "r") as fp:
-                output.des = torch.from_numpy(fp["descriptors"][()])
-
-        return output
-
-    @property
-    def nkpts(self) -> int:
-        return self.kpts.shape[0]
-
-    def keys(self) -> List[str]:
-        return list(self.__dict__.keys())
 
 
 class MethodWrapper(ABC):
@@ -224,12 +141,6 @@ class MethodWrapper(ABC):
         )
         output = output.mask(valid_mask)
         return output
-
-    def model_summary(self):
-        """
-        I would be nice to have this to print all torchinfo.summary infos
-        """
-        raise NotImplementedError
 
     def grid_sample_nan(
         self, xy: Tensor, img: Tensor, mode="nearest"
@@ -322,3 +233,11 @@ class MethodWrapper(ABC):
         if self.matcher is None:
             raise ValueError("No matcher defined for this wrapper")
         return self.matcher.match(des0, des1)
+
+    def extract_multiscale(
+        self,
+        img: Union[Tensor, np.ndarray],
+        max_kpts: Union[float, int],
+        scales: List[float],
+    ) -> List[MethodOutput]:
+        raise NotImplementedError
