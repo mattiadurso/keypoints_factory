b1a55274a15340153ac91511353f318eff788015
mattiadurso <mattiadurso98@gmail.com>
2025-09-18 14:44:17 +0200

    fix

--- bash/download_imc.sh
+++ bash/download_imc.sh
@@ -2,26 +2,18 @@
 # web page for images and gt: https://www.cs.ubc.ca/research/image-matching-challenge/2021/data/
 # btw for some reason, the first link downloads all the images (9x100), 
 # while the second has gt for all images but is missing: 
-# - 1 image in london_bridge
+# - 1 image  in london_bridge
 # - 2 images in florence_cathedral_side
 # - 3 images in piazza_san_marco
-# I manually fixed this and upload a corrected version downloadable with this script.
+# I manually fixed this and upload a corrected version downloadable with this script. I also add the validation set.
 
 
-# validation set
-wget -c -O benchmarks/imc/validation.zip \
-  "https://www.cs.ubc.ca/research/kmyi_data/imc2021-public/imc-2021-validation-phototourism.tar.gz"
-mkdir -p benchmarks/imc/data/phototourism/
-tar -xvzf benchmarks/imc/validation.zip -C benchmarks/imc/data/phototourism/
-rm benchmarks/imc/validation.zip
-
-
-# test set
+# test and val set
 wget -c -O benchmarks/imc/phototourism.zip \
-  "https://cloud.tugraz.at/index.php/s/AdD9XoXa5fmCWwN/download"
+  "https://cloud.tugraz.at/index.php/s/PTgmwHpkJ5sP3cG/download"
   
 mkdir -p benchmarks/imc/data
-unzip benchmarks/imc/phototourism.zip -d benchmarks/imc/data/phototourism
+unzip benchmarks/imc/phototourism.zip -d benchmarks/imc/data
 rm benchmarks/imc/phototourism.zip
 
 
