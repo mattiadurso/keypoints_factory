2fbb9a96715251aa8a38b9c5772a97e59da2c3ab
mattiadurso <mattiadurso98@gmail.com>
2025-09-19 23:27:12 +0200

    simplified

--- benchmarks/imc/run.py
+++ benchmarks/imc/run.py
@@ -1,5 +1,6 @@
 import os
 
+
 wrappers_list = [
     "aliked",
     "disk",
@@ -12,55 +13,30 @@ wrappers_list = [
 # Choose the benchmark script to run
 script_name = "benchmarks/imc/imc_benchmark.py"
 
-
-# for this benchmark, some methods declared some parameters. If not, use default ones
-sandesc = "-sandesc"
-params = { # after validation tuning, these are the best params
-    "disk": {"th": 0.75, "min_score": 0, "ratio_test": 0.95},
-    "disk-sandesc": {"th": 0.75, "min_score": 0, "ratio_test": 0.95},
-}
-
 scene_set = "val"  # test, val
+min_score = 0.0
+ratio_test = 1.0
+th = 1.0
+max_kpts = 2048
+multiview = False
 
-if scene_set == "val":
-    for th in sorted([0.75, 0.85, 0.95, 1.0]):
-        for ratio_test in sorted([0.95, 0.98, 1]):
-            for min_score in sorted([0, 0.5]):
-                for wrapper_name in wrappers_list:
-                    os.system(
-                        f"python {script_name} --wrapper-name {wrapper_name} --max-kpts 2048 \
-                            --th {th} --min-score {min_score} --ratio-test {ratio_test} \
-                            --scene-set {scene_set}"
-                    )
+for wrapper_name in wrappers_list:
+    os.system(
+        f"python {script_name} --wrapper-name {wrapper_name} --max-kpts 2048 \
+            --th {th} --min-score {min_score} --ratio-test {ratio_test} \
+            --scene-set {scene_set} --multiview {multiview}"
+    )
 
-                    # SANDesc is not published yet
-                    os.system(
-                        f"python {script_name} --wrapper-name {wrapper_name} --max-kpts 2048 \
-                        --th {th} --min-score {min_score} --ratio-test {ratio_test} \
-                        --scene-set {scene_set} --custom-desc sandesc_models/{wrapper_name}/final.pth"
-                    )
+    # SANDesc is not published yet
+    os.system(
+        f"python {script_name} --wrapper-name {wrapper_name} --max-kpts 2048 \
+        --th {th} --min-score {min_score} --ratio-test {ratio_test} --multiview {multiview} \
+        --scene-set {scene_set} --custom-desc sandesc_models/{wrapper_name}/final.pth"
+    )
+    i += 1  # <--- increment after running
 
-    os.system("rm -rf benchmarks/imc/benchmark-results")  # heavy files and not needed
-    os.system("rm -rf benchmarks/imc/benchmark-visualization")  # I don't use them
-
-elif scene_set == "test":
-    for wrapper_name in wrappers_list:
-        os.system(
-            f"python {script_name} --wrapper-name {wrapper_name} --max-kpts 2048 \
-                --th {params[wrapper_name]['th']} --min-score {params[wrapper_name]['min_score']} \
-                --ratio-test {params[wrapper_name]['ratio_test']} --scene-set {scene_set} \
-                "
-        )
-
-        # SANDesc is not published yet
-        os.system(
-           f"python {script_name} --wrapper-name {wrapper_name} --max-kpts 2048 \
-            --th {params[wrapper_name+sandesc]['th']} --min-score {params[wrapper_name+sandesc]['min_score']} \
-            --ratio-test {params[wrapper_name+sandesc]['ratio_test']} --scene-set {scene_set} \ 
-            --custom-desc sandesc_models/{wrapper_name}/final.pth"
-        )
-
-    os.system("rm -rf benchmarks/imc/benchmark-results")  # heavy files and not needed
+os.system("rm -rf benchmarks/imc/benchmark-results")  # heavy files and not needed
+os.system("rm -rf benchmarks/imc/benchmark-visualization")  # I don't use them
 
 
 ## Eventually it's possible to delete also the computed matches since it will
