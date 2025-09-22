211d229d578aa9b90622ae1ada2e4601a7e2232d
mattiadurso <mattiadurso98@gmail.com>
2025-09-22 16:06:44 +0200

    fixed link

--- bash/download_ghr.sh
+++ bash/download_ghr.sh
@@ -1,7 +1,7 @@
 wget -c -O benchmarks/graz_high_res/GrazHighResolution.zip \
-  "https://cloud.tugraz.at/index.php/s/4RZLnbbbTSjcjHc/download"
+  "https://cloud.tugraz.at/index.php/s/pWq9q62c9g5npCG/download"
 
 mkdir -p benchmarks/graz_high_res/data
-unzip benchmarks/graz_high_res/GrazHighResolution.zip -d benchmarks/graz_high_res/data
+unzip benchmarks/graz_high_res/GrazHighResolution.zip -d benchmarks/graz_high_res
 rm benchmarks/graz_high_res/GrazHighResolution.zip
   
